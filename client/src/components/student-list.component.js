c408036308c0a9ecd1de4cd7836492dfee1a742d
ManuelSong <sibarnyn@gmail.com>
2021-10-08 16:12:07 -0400

    final1

--- client/src/components/student-list.component.js
+++ client/src/components/student-list.component.js
@@ -14,7 +14,7 @@ export default class StudentList extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/students/')
+    axios.get('/students/')
       .then(res => {
         this.setState({
           students: res.data
